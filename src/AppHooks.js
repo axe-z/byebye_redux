2d15cf9450746ec22157f4371cab89f39e2ed214
axe-z <info@axe-z.com>
2019-03-28 16:25:25 -0400

    cool stuff

--- src/AppHooks.js
+++ src/AppHooks.js
@@ -1,105 +1,133 @@
 import React, { useReducer, useContext, useEffect, useRef } from 'react';
 
+function appReducer(state, action){
+	//type est le premier arg , payload est le deuxieme arg, comme pour redux
+	switch(action.type) {
+		case "reset" : {
+			console.log(action, state)
+			return  action.payload
 
-
-function appReducer(state, action) {
-  switch (action.type) {
-    case 'reset': {
-      return action.payload;
-    }
-    case 'ajout': { //retourne un array , ...state(ce qu on avait, et l ajout)
-      return [
-        ...state,
-        {
-          id: Date.now(),
-          text: '',
-          complet: false,
-        },
-      ];
-    }
-    case 'deleter': {
-      return state.filter(item => item.id !== action.payload);
-    }
-    case 'completer': {
+		}
+		case "ajout" : {
+			console.log(action, state)
+			return [
+				...state,
+				{
+					id: Date.now(),
+					text: '',
+					completer: false
+				}
+			]
+		}
+		case "detruit" :  {
+			return state.filter(item => item.id !== action.payload)
+		}
+		case 'completer': {
       return state.map(item => {
         if (item.id === action.payload) {
           return {
             ...item,
-            complet: !item.completed,
+            completer: !item.completer,
           };
         }
         return item;
       });
     }
-    default: {
-      return state;
-    }
-  }
+		default: {
+			return state
+		}
+	}
 }
-
 const Context = React.createContext();
 
-function useEffectOnce(cb) {
-  const didRun = useRef(false);
 
-  useEffect(() => {
-    if (!didRun.current) {
-      cb();
-      didRun.current = true;
-    }
-  });
+function UtiliseEffectOnce (cb) {
+	const aRouler = useRef(false)
+	useEffect(() => {
+		if (!aRouler.current) {
+			cb()
+			aRouler.current = true;
+		}
+	});
 }
 
-export default function TodosApp() {
-  const [state, dispatch] = useReducer(appReducer, []);
 
-  useEffectOnce(() => {
-    const raw = localStorage.getItem('data');
-    dispatch({ type: 'reset', payload: raw ? JSON.parse(raw) : [] });
-  });
 
-  useEffect(
-    () => {
-      localStorage.setItem('data', JSON.stringify(state));
-    },
-    [state]
-  );
+export default function TodoApp() {
+  const [state, dispatch] = useReducer(appReducer, []); //state = [ ] dispatch = fn qui prend le type
+	//console.log(state) //[]
+
+
+	// const aRouler = useRef(false) //donne un wrapper.current
+
+
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(state));
+	}, [state]);  // roule quand state change
+
+	UtiliseEffectOnce(() => {
+		const debutData = localStorage.getItem("data");
+		dispatch({ type: "reset", payload: debutData ? JSON.parse(debutData) : [] });
+	 });
+	// useEffect(() => {
+	// 	if(!aRouler.current){
+  //   const debutData = localStorage.getItem("data");
+	// 	dispatch({ type: "reset", payload: JSON.parse(debutData) });
+	// 	aRouler.current = !aRouler.current;
+	// }
+	// }, []); // vide , roule juste un fois
+
 
   return (
     <Context.Provider value={dispatch}>
-      <h1>Todos App</h1>
-      <button onClick={() => dispatch({ type: 'add' })}>New Todo</button>
-      <br />
-      <br />
-      <TodosList items={state} />
+      <div>
+        <h1>To do App</h1>
+        <button onClick={() => dispatch({ type: "ajout" })}>
+          Nouveau Todo
+        </button>{" "}
+        {/* pas besoin de payload */}
+        <TodoList items={state} />
+      </div>
     </Context.Provider>
   );
 }
 
-function TodosList({ items }) {
-  return items.map(item => <TodoItem key={item.id} {...item} />);
+ function TodoList({items}){
+	return (
+		<div>
+			{items.map(item => (
+				<div key={item.id}>
+				<TodoItem  {...item}/>
+				</div>
+			))}
+		</div>
+	)
 }
 
-
-function TodoItem({ id, completed, text }) {
-  const dispatch = useContext(Context);
+function TodoItem({ id, completer, text }) {
+	const dispatch = useContext(Context); //au lieu d utiliser le context.Consumer , on a juste dispatch
+//  console.log(completer)
   return (
     <div
       style={{
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'space-between',
+				justifyContent: 'space-between',
+				width: 600
       }}
     >
+		<br/>
+		<br/>
       <input
         type="checkbox"
-        checked={completed}
-        onChange={() => dispatch({ type: 'completed', payload: id })}
+				 checked={completer}
+				 onChange={() => dispatch({ type: 'completer', payload: id })}
       />
-      <input type="text" defaultValue={text} />
-      <button onClick={() => dispatch({ type: 'delete', payload: id })}>
-        Delete
+      <input type="text" defaultValue={text} style={completer ? {color: 'red'}: null}/>
+      <button onClick={() => dispatch({ type: 'detruit', payload: id })}>
+			detruire
       </button>
     </div>
   );
 }
+
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import AppHooks from './AppHooks';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppHooks />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
